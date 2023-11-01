f052ffc779c0d89c478fb6ecf2d7e63f5fbbf229
Suruthi21ITR110 <118598974+Suruthi21ITR110@users.noreply.github.com>
2023-11-01 14:42:41 +0530

    Update LoginPage.jsx

--- src/components/Header/LoginPage.jsx
+++ src/components/Header/LoginPage.jsx
@@ -193,6 +193,99 @@
 // }
 
 // export default LoginPage;
+// import React, { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+// import './LoginPage.scss';
+
+// function LoginPage() {
+//   const [username, setUsername] = useState('');
+//   const [password, setPassword] = useState('');
+//   const navigate = useNavigate();
+
+//   const handleLogin = async (e) => {
+//     e.preventDefault();
+
+//     if (!username || !password) {
+//       alert('Please fill in all fields');
+//       return;
+//     }
+
+//     try {
+//       const response = await fetch('/coachlog', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ username, password }),
+//       });
+
+//       if (response.ok) {
+//         const data = await response.json();
+//         localStorage.setItem('token', data.token);
+        
+//         if (password === 'suru') {
+//           alert('Special Login Success!');
+//         } else {
+//           alert('Login Successful');
+//         }
+//         navigate('/');
+//       } else {
+//         alert('Invalid username or password');
+//       }
+//     } catch (error) {
+//       console.error('Error:', error);
+//       alert('An error occurred during login.');
+//     }
+//   };
+
+//   const backgroundImageUrl = 'https://img.freepik.com/free-vector/botanical-themed-design-space_53876-89210.jpg?w=1060&t=st=1697041206~exp=1697041806~hmac=8e6b84513540e643b1d2b818adfc2bd5687e6ca855f3d51c720228bb72505b2e';
+
+//   return (
+//     <div className='log'  style={{
+//       backgroundImage: `url(${backgroundImageUrl})`,
+//       backgroundSize: 'cover',
+//       backgroundPosition: 'center',
+//     }}>
+//       <div className="login-container">
+//         <div className="login-content">
+//           <div className="login-form">
+//             <h2>Sign In</h2>
+//             <p>“Relax we've got this!”</p>
+//             <form onSubmit={handleLogin}>
+//               <div className="form-group">
+//                 <input
+//                   type="text"
+//                   id="input1"
+//                   name="input1"
+//                   required
+//                   placeholder="Username"
+//                   value={username}
+//                   onChange={(e) => setUsername(e.target.value)}
+//                 />
+//               </div>
+//               <div className="form-group">
+//                 <input
+//                   type="password"
+//                   id="input2"
+//                   name="input2"
+//                   required
+//                   placeholder="Password"
+//                   value={password}
+//                   onChange={(e) => setPassword(e.target.value)}
+//                 />
+//               </div>
+//               <button type="submit">Login</button><br/><br/>
+//               Don't have an account? <Link to='/SignupPage' style={{color:"#007FFF", textDecoration:"none", fontSize: "15px"}}>Create a new account</Link>
+//             </form>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default LoginPage;
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
@@ -211,39 +304,22 @@ function LoginPage() {
       return;
     }
 
-    try {
-      const response = await fetch('/coachlog', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        localStorage.setItem('token', data.token);
-        
-        if (password === 'suru') {
-          alert('Special Login Success!');
-        } else {
-          alert('Login Successful');
-        }
-        navigate('/');
-      } else {
-        alert('Invalid username or password');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      alert('An error occurred during login.');
+    // Replace this hardcoded password check with your secure authentication method
+    if (password === 'subhi') {
+      // For demo purposes, the password 'subhi' grants access
+      localStorage.setItem('token', 'yourGeneratedTokenHere');
+      alert('Login Successful');
+      navigate('/');
+    } else {
+      alert('Invalid username or password');
     }
   };
 
-  const backgroundImageUrl = 'https://img.freepik.com/free-vector/botanical-themed-design-space_53876-89210.jpg?w=1060&t=st=1697041206~exp=1697041806~hmac=8e6b84513540e643b1d2b818adfc2bd5687e6ca855f3d51c720228bb72505b2e';
+  const backgroundImageUrl = 'https://img.freepik.com/free-photo/empty-modern-room-with-furniture_23-2149178345.jpg?w=1060&t=st=1697099305~exp=1697099905~hmac=b7217fcf8e37934360faa7ee0becb0e69a816253118e53683ddc39bb302e385f'; // Replace with the actual image URL
 
   return (
-    <div className='log'  style={{
-      backgroundImage: `url(${backgroundImageUrl})`,
+    <div className='log' style={{
+      backgroundImage: `url(${backgroundImageUrl})`, // Set the online image URL
       backgroundSize: 'cover',
       backgroundPosition: 'center',
     }}>
@@ -275,8 +351,12 @@ function LoginPage() {
                   onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
-              <button type="submit">Login</button><br/><br/>
-              Don't have an account? <Link to='/SignupPage' style={{color:"#007FFF", textDecoration:"none", fontSize: "15px"}}>Create a new account</Link>
+              <button type="submit">Login</button>
+              <br/><br/>
+              Don't have an account?
+              <Link to='/SignupPage' style={{color:"#007FFF", textDecoration:"none", fontSize: "15px"}}>
+                Create a new account
+              </Link>
             </form>
           </div>
         </div>
@@ -286,4 +366,6 @@ function LoginPage() {
 }
 
 export default LoginPage;
+
+
 
